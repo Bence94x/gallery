55408a36ac8ed90d7951d012aa3291d14a0a01f2
Bence94x <bence94x@gmail.com>
2021-07-23 13:31:32 +0200

    change object to array

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-let crabPhoto = {
-    photo: "images/crab.jpg",
-    title: "Crab",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero delectus tenetur totam possimus eaque neque necessitatibus consequatur sunt quo corporis.",
-};
+let data = [
+    {
+        "photo": "images/crab.jpg",
+        "title": "Crab",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero delectus tenetur totam possimus eaque neque necessitatibus consequatur sunt quo corporis.",
+    },
+    {
+        "photo": "images/fish.jpeg",
+        "title": "Fish",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero delectus tenetur totam possimus eaque neque necessitatibus consequatur sunt quo corporis.",
+    },
+    {
+        "photo": "images/marines.jpeg",
+        "title": "Marines",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero delectus tenetur totam possimus eaque neque necessitatibus consequatur sunt quo corporis.",
+    },
+    {
+        "photo": "images/turtle.jpeg",
+        "title": "Turtle",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero delectus tenetur totam possimus eaque neque necessitatibus consequatur sunt quo corporis.",
+    },
+    {
+        "photo": "images/turtle2.jpeg",
+        "title": "Another turtle",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero delectus tenetur totam possimus eaque neque necessitatibus consequatur sunt quo corporis.",
+    },
+    {
+        "photo": "images/whale.jpeg",
+        "title": "Whale",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero delectus tenetur totam possimus eaque neque necessitatibus consequatur sunt quo corporis.",
+    },
+]
 
-$(".photo").attr("src", crabPhoto.photo);
-$(".title").html(crabPhoto.title);
-$(".description").html(crabPhoto.description);
+let currentPhoto = 0;
+$(".photo").attr("src", data[4].photo);
+$(".title").html(data[4].title);
+$(".description").html(data[4].description);
